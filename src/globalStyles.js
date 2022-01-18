397deb34feba1a7823648979112fe2fb20b62320
omadde40 <omadde40@gmail.com>
2022-01-18 15:13:18 +0530

    first commit

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,8 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 
-
-
-
 const GlobalStyle = createGlobalStyle`
 
 *,*::before,*::after,h1,h2,h3,h4,h5,h6{
@@ -13,14 +10,11 @@ h1,h2,h3,h4,h5,h6{
     display: inline-block;
 }
 
-
 body{
     margin: 0;
     padding: 0;
     overflow-x: hidden;
     font-family: 'Source Sans Pro',sans-serif;
-}
-
-`
+}`;
 
 export default GlobalStyle;
